d7107899acb10995dc284a1ada7886662eb9baa7
Steven Shonts <steven@dv01.co>
2018-08-04 13:26:52 -0400

    more stuff

--- load.py
+++ load.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
 
-loan_path = "/Users/shonts/Documents/kaggle/loan.csv"
+loan_path = "loan.csv"
 
 loan = pd.read_csv(loan_path, low_memory = False)
 
 # Filter df to 36 month loans. n.b. leading space.
 loan_36 = loan[loan.term == " 36 months"]
 
-# issue_d is a date string like Mar-12. Parse as pandas DateTime
-loan_36['issue_dt'] = pd.to_datetime(df['date'], format='%b-%y')
+# issue_d is a date string like Mar-2012. Parse as pandas DateTime
+loan_36['issue_dt'] = pd.to_datetime(loan_36['issue_d'], format='%b-%Y')
 
 # filter to loans with complete history.
 # note that Charged Off or Prepayed loans may have less that 36 months of history.
-loan_36_ret = loan[loan_36.loan_status.isin(['Fully Paid', 'Charged Off', 'Default'])]
+loan_36_ret = loan_36[loan_36['issue_dt'] <= '2014-01-01']
 
 # # calculate total cash returned to investor on loan
 # loan_36_ret['cash_in'] = loan_36_ret[loan_36_ret['total_rec_prncp'] + loan_36_ret['total_rec_int'] + loan_36_ret['total_rec_late_fee']]
 
 # total return is defined as (cash in - cash invested) / (cash invested)
-loan_36_ret['total_return'] = loan_36_ret[(loan_36_ret['total_pymnt_inv'] - loan_36_ret['funded_amnt_inv'])]
+loan_36_ret['total_return'] = loan_36_ret[(loan_36_ret['total_pymnt_inv'] - loan_36_ret['funded_amnt_inv']) / loan_36_ret['funded_amnt_inv']]
 
 # annualized return assumes holding period of 3 years (even if loan prepays or charged off)
 # this underestimates returns, as prepayed principal could be re-invested
@@ -34,8 +34,6 @@ loan_36.groupby('grade').agg({
 		'total_rec_late_fee': 'sum',
 		'annualized_return' : 'mean'
 	})
-
-
 
 
 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# RUNNING SCRIPTS
+
+you will need to place a loan file called loan.csv in the root directory.
+
+load.py will load this csv and add the columns relevant for the analysis.
+
+analyse.py will perform some basic aggregations and create a few plots using matplotlib.
+
+model.py will create a model of returns based on certain selected origination characteristics, and attempt to validate the results using a testing set of loans.
+
