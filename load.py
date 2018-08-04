036f5abf769cb41601abbc19be648982a239b753
Steven Shonts <steven@dv01.co>
2018-08-04 12:19:22 -0400

    init

---
+++ load.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+loan_path = "/Users/shonts/Documents/kaggle/loan.csv"
+
+loan = pd.read_csv(loan_path, low_memory = False)
+
+# Filter df to 36 month loans. n.b. leading space.
+loan_36 = loan[loan.term == " 36 months"]
+
+# issue_d is a date string like Mar-12. Parse as pandas DateTime
+loan_36['issue_dt'] = pd.to_datetime(df['date'], format='%b-%y')
+
+# filter to loans with complete history.
+# note that Charged Off or Prepayed loans may have less that 36 months of history.
+loan_36_ret = loan[loan_36.loan_status.isin(['Fully Paid', 'Charged Off', 'Default'])]
+
+# # calculate total cash returned to investor on loan
+# loan_36_ret['cash_in'] = loan_36_ret[loan_36_ret['total_rec_prncp'] + loan_36_ret['total_rec_int'] + loan_36_ret['total_rec_late_fee']]
+
+# total return is defined as (cash in - cash invested) / (cash invested)
+loan_36_ret['total_return'] = loan_36_ret[(loan_36_ret['total_pymnt_inv'] - loan_36_ret['funded_amnt_inv'])]
+
+# annualized return assumes holding period of 3 years (even if loan prepays or charged off)
+# this underestimates returns, as prepayed principal could be re-invested
+loan_36_ret['annualized_return'] = loan_36_ret[(loan_36_ret['total_return'] + 1) ^ (1.0/3)]
+
+loan_36.groupby('grade').agg({
+		'funded_amnt_inv': ['min', 'mean', 'max', 'sum'],
+		'int_rate': 'mean',
+		'cash_in' : 'sum',
+		'total_rec_prncp': 'sum',
+		'total_rec_int': 'sum',
+		'total_rec_late_fee': 'sum',
+		'annualized_return' : 'mean'
+	})
+
+
+
+
+
+
+num_loans = len(loan_36)
+loan_36['sample'] = p.Series(np.random.randn(sLength), index=df1.index)
